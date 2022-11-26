07ca31e1a0d9d5524cdd871c52cd27068e6d2cfa
sania070306 <alsanade.s@gmail.com>
2022-11-26 15:23:52 +1100

    fifth done

--- posts/urls.py
+++ posts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('hasthags/', hashtags_view),
     path('posts/', posts_view),
     path('posts/<int:id>/', post_detail_view),
-    path('posts/create/', post_create_view)
+    path('posts/create/', post_create_view),
 ]
