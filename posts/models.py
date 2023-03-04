9158005ef664db8a27ffdfddcdfbc18b68c9eadb
sania070306 <alsanade.s@gmail.com>
2023-03-04 18:04:14 +1100

    p

--- posts/models.py
+++ posts/models.py
@@ -29,4 +29,5 @@ class Comment(models.Model):
     text = models.TextField()
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
-
+    def __str__(self):
+        return f'{self.author.username} {self.text}'
